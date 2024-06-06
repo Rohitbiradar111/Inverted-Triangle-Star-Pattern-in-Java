deddb249d96c020a482bca34d0989f18d1b6a540
RohitBiradar <rohitpramilabiradar@gmail.com>
2024-06-06 17:18:34 +0530

    add this java file

---
+++ InvertedTrianglePattern.java
@@ -0,0 +1,16 @@
+//This program prints a Inverted Triangle Star Pattern in Java
+
+public class InvertedTrianglePattern {
+    public static void main(String args[])
+    {
+        int n = 5;
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=i;j<=n;j++)
+            {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
